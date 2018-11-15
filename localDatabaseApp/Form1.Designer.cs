b6763971946c767d06eb368d74beaf9ef2fb9f82
Melisa <melissalantigua@hotmail.com>
2018-11-15 13:13:39 -0400

    agregar users

--- localDatabaseApp/Form1.Designer.cs
+++ localDatabaseApp/Form1.Designer.cs
@@ -32,6 +32,15 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.Search = new System.Windows.Forms.Button();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.labelMatricula = new System.Windows.Forms.Label();
+            this.matriculaInt = new System.Windows.Forms.TextBox();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.labelNombre = new System.Windows.Forms.Label();
+            this.textApellido = new System.Windows.Forms.TextBox();
+            this.labelApellido = new System.Windows.Forms.Label();
+            this.textPass = new System.Windows.Forms.TextBox();
+            this.labelPass = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // usuarios
@@ -69,17 +78,101 @@
             this.Search.UseVisualStyleBackColor = true;
             this.Search.Click += new System.EventHandler(this.Search_Click);
             // 
+            // buttonInsert
+            // 
+            this.buttonInsert.Location = new System.Drawing.Point(568, 341);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(119, 44);
+            this.buttonInsert.TabIndex = 4;
+            this.buttonInsert.Text = "Insertar";
+            this.buttonInsert.UseVisualStyleBackColor = true;
+            this.buttonInsert.Click += new System.EventHandler(this.buttonInsert_Click);
+            // 
+            // labelMatricula
+            // 
+            this.labelMatricula.AutoSize = true;
+            this.labelMatricula.Location = new System.Drawing.Point(466, 167);
+            this.labelMatricula.Name = "labelMatricula";
+            this.labelMatricula.Size = new System.Drawing.Size(131, 32);
+            this.labelMatricula.TabIndex = 5;
+            this.labelMatricula.Text = "matricula";
+            this.labelMatricula.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // matriculaInt
+            // 
+            this.matriculaInt.Location = new System.Drawing.Point(625, 167);
+            this.matriculaInt.Name = "matriculaInt";
+            this.matriculaInt.Size = new System.Drawing.Size(158, 38);
+            this.matriculaInt.TabIndex = 6;
+            // 
+            // textNombre
+            // 
+            this.textNombre.Location = new System.Drawing.Point(625, 211);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.Size = new System.Drawing.Size(158, 38);
+            this.textNombre.TabIndex = 8;
+            // 
+            // labelNombre
+            // 
+            this.labelNombre.AutoSize = true;
+            this.labelNombre.Location = new System.Drawing.Point(466, 211);
+            this.labelNombre.Name = "labelNombre";
+            this.labelNombre.Size = new System.Drawing.Size(115, 32);
+            this.labelNombre.TabIndex = 7;
+            this.labelNombre.Text = "Nombre";
+            // 
+            // textApellido
+            // 
+            this.textApellido.Location = new System.Drawing.Point(625, 255);
+            this.textApellido.Name = "textApellido";
+            this.textApellido.Size = new System.Drawing.Size(158, 38);
+            this.textApellido.TabIndex = 10;
+            // 
+            // labelApellido
+            // 
+            this.labelApellido.AutoSize = true;
+            this.labelApellido.Location = new System.Drawing.Point(466, 255);
+            this.labelApellido.Name = "labelApellido";
+            this.labelApellido.Size = new System.Drawing.Size(119, 32);
+            this.labelApellido.TabIndex = 9;
+            this.labelApellido.Text = "Apellido";
+            // 
+            // textPass
+            // 
+            this.textPass.Location = new System.Drawing.Point(625, 299);
+            this.textPass.Name = "textPass";
+            this.textPass.Size = new System.Drawing.Size(158, 38);
+            this.textPass.TabIndex = 12;
+            // 
+            // labelPass
+            // 
+            this.labelPass.AutoSize = true;
+            this.labelPass.Location = new System.Drawing.Point(466, 299);
+            this.labelPass.Name = "labelPass";
+            this.labelPass.Size = new System.Drawing.Size(162, 32);
+            this.labelPass.TabIndex = 11;
+            this.labelPass.Text = "Contraseña";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(868, 637);
+            this.Controls.Add(this.textPass);
+            this.Controls.Add(this.labelPass);
+            this.Controls.Add(this.textApellido);
+            this.Controls.Add(this.labelApellido);
+            this.Controls.Add(this.textNombre);
+            this.Controls.Add(this.labelNombre);
+            this.Controls.Add(this.matriculaInt);
+            this.Controls.Add(this.labelMatricula);
+            this.Controls.Add(this.buttonInsert);
             this.Controls.Add(this.Search);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.usuarios);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
             this.Text = "Sql database access";
             this.ResumeLayout(false);
@@ -93,6 +186,15 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button Search;
+        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.Label labelMatricula;
+        private System.Windows.Forms.TextBox matriculaInt;
+        private System.Windows.Forms.TextBox textNombre;
+        private System.Windows.Forms.Label labelNombre;
+        private System.Windows.Forms.TextBox textApellido;
+        private System.Windows.Forms.Label labelApellido;
+        private System.Windows.Forms.TextBox textPass;
+        private System.Windows.Forms.Label labelPass;
     }
 }
 
--- localDatabaseApp/Form1.cs
+++ localDatabaseApp/Form1.cs
@@ -7,11 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
 
 namespace localDatabaseApp
 {
     public partial class Form1 : Form
     {
+        List<students> users = new List<students>();
+        SqlCommand cmd;
+        SqlConnection con;
+        SqlDataAdapter da;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +26,25 @@ namespace localDatabaseApp
 
         private void Search_Click(object sender, EventArgs e)
         {
+            accesoData db = new accesoData();
+            users = db.GetUsers(textBox1.Text);
+        }
 
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonInsert_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Melisa\Desktop\localDatabaseApp\localDatabaseApp\students.mdf;Integrated Security=True");
+            con.Open();
+            cmd = new SqlCommand("INSERT INTO estudiantes (matricula,nombre,apellido,contraseña,total_ejercicios,date_reporte_generado) VALUES (@matricula,@nombre,@apellido,@contraseña,NULL,NULL)", con);
+            cmd.Parameters.Add("@matricula", matriculaInt.Text);
+            cmd.Parameters.Add("@nombre", textNombre.Text);
+            cmd.Parameters.Add("@apellido", textApellido.Text);
+            cmd.Parameters.Add("@contraseña", textPass.Text);
+            cmd.ExecuteNonQuery(); 
         }
     }
 }
